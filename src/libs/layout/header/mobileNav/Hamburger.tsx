01478a3139020d8220d0e9b24c60b64abaaf66c3
Iqbal Nurkholis <iqbal132nurkholis@gmail.com>
2022-08-10 22:34:48 +0700

    add aria-label

--- src/libs/layout/header/mobileNav/Hamburger.tsx
+++ src/libs/layout/header/mobileNav/Hamburger.tsx
@@ -9,17 +9,19 @@ type HamburgerProps = {
 
 export function Hamburger ({ onClick }: HamburgerProps) {
   return (
-    <button css={css({
-      padding: '5px',
-      marginLeft: 'auto',
-      background: 'transparent',
-      borderRadius: '5px',
-      border: '1px solid #ddd',
-      [mq['md']]: {
-        display: 'none'
-      }
-    })}
+    <button
+      aria-label="Open Mobile Menu"
       onClick={onClick}
+      css={css({
+        padding: '5px',
+        marginLeft: 'auto',
+        background: 'transparent',
+        borderRadius: '5px',
+        border: '1px solid #ddd',
+        [mq['md']]: {
+          display: 'none'
+        }
+      })}
     >
       <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" css={css({ display: 'block' })} viewBox="0 0 16 16">
         <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
